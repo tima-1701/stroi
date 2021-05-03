55ddb5bc4cf4c9398c6c98645161dd014ec6558f
tima-1701 <tima-1701@mail.ru>
2021-05-03 13:56:46 +0600

    test

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
       autoplay: true,
     });
   });
+  $('.services__tabs').each(function() {
+    let ths = $(this);
+    ths.find('.tabs__block').not(':first').hide();
+    ths.find('.tabs__item').click(function() {
+      ths.find('.tabs__item').removeClass('active').eq($(this).index()).addClass('active');
+      ths.find('.tabs__block').hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass('active');
+  });
