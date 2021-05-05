44ec3adfc3f37d324633e3d3f35db5dbf01521b1
tima-1701 <tima-1701@mail.ru>
2021-05-05 13:12:20 +0600

    test

--- app/js/main.js
+++ app/js/main.js
@@ -13,3 +13,23 @@ $(document).ready(function(){
       ths.find('.tabs__block').hide().eq($(this).index()).fadeIn()
     }).eq(0).addClass('active');
   });
+
+  $({blurRadius: 5}).animate({blurRadius: 0}, {
+    duration: 5000,
+    easing: 'swing',
+    step: function() {
+      $(".counter__numb").css({
+        "-webkit-filter": "blur("+this.blurRadius+"px)",
+        "filter": "blur("+this.blurRadius+"px)"
+      });
+    }
+  });
+  var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
+  $(".counter__numb").each(function() {
+    var tcount = $(this).data("count");
+    $(this).animateNumber({ number: tcount,
+      easing: 'easeInQuad',
+      "font-size": "40px",
+      numberStep: comma_separator_number_step},
+      5000);
+  });
